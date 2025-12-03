92d4f5298443b974fe37e4beb96b35993aa09e5b
Asfanchik <114120335+Asfanchik@users.noreply.github.com>
2025-12-03 15:00:05 +0900

    Create main.py

---
+++ day 9/main.py
@@ -0,0 +1,50 @@
+# programming_dictionari = {
+#   "Bug": "An error in a program that prevents the program from runnungas expected.",
+#   "Function": "A piece of code that you can easily call over again.",
+#   "Loop":"The action of doing something over and over again."  
+# }
+
+# # Showing information from dictionary
+# # print(programming_dictionari["Bug"])
+
+# # # Adding information to dictionary
+# # programming_dictionari["programing"]= "That what I love"
+# # print(programming_dictionari)
+
+# #Creat an empety dictionary
+# empety_dictionary = {}
+
+# # #WIpe an existing dictionary
+# # programming_dictionari = {}
+# # print(programming_dictionari)
+
+# # #Edit an item in a dictionary
+# # programming_dictionari["Bug"] = "A mous on computer"
+# # print(programming_dictionari)
+
+# # Loop though a dictionary
+# for word in programming_dictionari:
+#     print(word)
+#     print(programming_dictionari[word])
+
+# student_score = {
+#     "Harry " : 81, 
+#     "Ron" : 78,
+#     "Draco": 62,
+#     "Hermione" : 99
+# }
+
+# student_grades = {}
+
+# for students in student_score:
+#     score = student_score[students]
+#     if score > 90:
+#         student_grades[students] = "Outstanding"
+#     elif score > 80:
+#         student_grades[students] = "Excceds Expections"
+#     elif score > 70:
+#         student_grades[students] = "Accaptable"
+#     else:
+#         student_grades[students] = "Fail"
+ 
+# print(student_grades)
