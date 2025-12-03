b3b0354186b3fc225cbd3b8dd3e4c87ec5a8e6ed
Asfanchik <114120335+Asfanchik@users.noreply.github.com>
2025-12-03 14:35:29 +0900

    Add files via upload

---
+++ day 1/1 day.py
@@ -0,0 +1,17 @@
+#a = input("a: ")
+#b = input("b: ")
+
+#c=a
+#a=b
+#b=c
+
+#print("a: " + a)
+#print("b: " + b)
+print("Welcom for may program")
+name = input("what is your name ? ")
+city = input("Where are you are from ? ")
+pet  = input("What is your pet ? ")
+print("Hello " + name)
+print("You are from " + city)
+print("You like " + pet)
+
