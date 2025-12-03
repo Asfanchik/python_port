7dad94fc68ae045b829e069ef7e79313d416ea13
Asfanchik <114120335+Asfanchik@users.noreply.github.com>
2025-12-03 15:16:29 +0900

    Create main.py

---
+++ day17/main.py
@@ -0,0 +1,24 @@
+# class User:
+#
+#     #pass #just skipng atributs
+#     def __init__(self, id_user, name_user, year_user):
+#         self.id = id_user
+#         self.name = name_user
+#         self.age = year_user
+#         self.followers = 0
+#         self.following = 0
+#     def follow(self, user):
+#         user.followers += 1
+#         user.following += 1
+#
+#
+# user1 = User("12534", "Jack", "25")
+# user2 = User("25879", "Anna", "21")
+#
+#
+# user1.follow(user2)
+# print(user1.followers)
+# print(user1.following)
+# print(user2.followers)
+# print(user2.following)
+
